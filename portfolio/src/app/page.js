1ba44fe3c0d95eff02c76f9627ab6b8e9f771f9a
kevin joel noviello <kjnoviello@gmail.com>
2024-10-29 10:02:17 -0300

    before refactor

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -4,7 +4,8 @@ import WordFadeIn from "@/components/ui/word-fade-in";
 import { GridPattern } from "@/components/ui/animated-grid-pattern";
 import { motion } from "framer-motion"
 import { useEffect, useState } from "react";
-
+import Link from "next/link";
+import './styles.css'
 
 export default function Home() {
 
@@ -13,6 +14,25 @@ export default function Home() {
   const [isHoveredPro, setIsHoveredPro] = useState(false);
   const [isHoveredAbo, setIsHoveredAbo] = useState(false);
   const [isResponsive, setIsResponsive] = useState(true)
+
+
+  useEffect(() => {
+    const fetchCard = async () => {
+      try {
+        const res = await fetch("/utils/intro.json")
+        if (!res.ok) {
+          throw new Error("Error al obtener los datos");
+        }
+        const data = await res.json()
+        setDataCard(data)
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchCard()
+  }, [])
+
 
   useEffect(() => {
     // Detectar tamaño de pantalla y ajustar `isResponsive`
@@ -70,126 +90,137 @@ export default function Home() {
 
         {/* Section cards */}
         <section className="flex flex-wrap justify-center gap-5">
-
           <div className="flex flex-col sm:flex-row gap-6" style={{ perspective: '1000px' }}>
-            <motion.article
-              className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
-              onHoverStart={() => setIsHoveredExp(true)}
-              onHoverEnd={() => setIsHoveredExp(false)}
-              animate={{
-                rotateZ: isHoveredExp ? 0 : 12,
-                rotateY: isHoveredExp ? 0 : 20,
-                rotateX: isHoveredExp ? 0 : -30,
-                scale: isHoveredExp ? 1 : 0.8,
-                boxShadow: isHoveredExp && !isResponsive ? '0px 0px 200px rgba(255, 255, 255, 0.9)' : '2px 8px 10px rgba(255, 255, 255, 0.9)'
-              }}
-            >
 
-              <div className="bg-black rounded-xl z-10">
-                <Image
-                  className="dark:invert w-[120px] h-[90px]"
-                  src="/home/experience.svg"
-                  alt="my photo"
-                  width={100}
-                  height={100}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col gap-2 px-2">
-                <p className="text-md text-[#58e6d9]"><strong>Experiencia</strong></p>
-                <p className="text-lg">Conoce más sobre mi carrera.</p>
-              </div>
-            </motion.article>
+            {/* {EXPERIENCE} */}
+            <Link href="/pages/experience" index className="w-full">
+              <motion.article
+                className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
+                onHoverStart={() => setIsHoveredExp(true)}
+                onHoverEnd={() => setIsHoveredExp(false)}
+                animate={{
+                  rotateZ: isHoveredExp ? 0 : -12,
+                  rotateY: isHoveredExp ? 0 : -10,
+                  rotateX: isHoveredExp ? 0 : -20,
+                  scale: isHoveredExp ? 1 : 0.8,
+                  boxShadow: isHoveredExp && !isResponsive ? '0px 0px 200px rgba(255, 255, 255, 0.9)' : '2px 8px 10px rgba(255, 255, 255, 0.9)'
+                }}
+              >
 
+                <div className="bg-black rounded-xl z-10">
+                  <Image
+                    className="dark:invert w-[120px] h-[90px]"
+                    src="/home/experience.svg"
+                    alt="my photo"
+                    width={100}
+                    height={100}
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col gap-2 px-2">
+                  <p className="text-md text-[#58e6d9]"><strong>Experiencia</strong></p>
+                  <p className="text-lg">Conoce más sobre mi carrera</p>
+                </div>
+              </motion.article>
+            </Link>
 
-            <motion.article
-              className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
-              onHoverStart={() => setIsHoveredEdu(true)}
-              onHoverEnd={() => setIsHoveredEdu(false)}
-              animate={{
-                rotateZ: isHoveredEdu ? 0 : -8,
-                rotateY: isHoveredEdu ? 0 : 20,
-                rotateX: isHoveredEdu ? 0 : 15,
-                scale: isHoveredEdu ? 1 : 0.8, // Ajustar escala en función del hover
-                boxShadow: isHoveredEdu && !isResponsive? '0px 0px 200px rgba(255, 255, 255, 0.9' : '2px 8px 10px rgba(255, 255, 255, 0.9)', // Cambiar sombra
-              }}
-            >
-              <div className="bg-black rounded-xl z-10">
-                <Image
-                  className="dark:invert w-[120px] h-[90px]"
-                  src="/home/education.svg"
-                  alt="my photo"
-                  width={120}
-                  height={100}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col gap-2 px-2">
-                <p className="text-md text-[#58e6d9]"><strong>Educación</strong></p>
-                <p>Mis estudios hasta hoy.</p>
-              </div>
-            </motion.article>
+            {/* {EDUCATION} */}
+            <Link href="/pages/education" index className="w-full">
+              <motion.article
+                className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
+                onHoverStart={() => setIsHoveredEdu(true)}
+                onHoverEnd={() => setIsHoveredEdu(false)}
+                animate={{
+                  rotateZ: isHoveredEdu ? 0 : -8,
+                  rotateY: isHoveredEdu ? 0 : 20,
+                  rotateX: isHoveredEdu ? 0 : 15,
+                  scale: isHoveredEdu ? 1 : 0.8, // Ajustar escala en función del hover
+                  boxShadow: isHoveredEdu && !isResponsive ? '0px 0px 200px rgba(255, 255, 255, 0.9' : '2px 8px 10px rgba(255, 255, 255, 0.9)', // Cambiar sombra
+                }}
+              >
+                <div className="bg-black rounded-xl z-10">
+                  <Image
+                    className="dark:invert w-[120px] h-[90px]"
+                    src="/home/education.svg"
+                    alt="my photo"
+                    width={100}
+                    height={100}
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col gap-2 px-2">
+                  <p className="text-md text-[#58e6d9]"><strong>Educación</strong></p>
+                  <p>Mis estudios hasta hoy.</p>
+                </div>
+              </motion.article>
+            </Link>
+
           </div>
-
           <div className="flex flex-col sm:flex-row gap-6 sm:mt-2" style={{ perspective: '1000px' }}>
-            <motion.article
-              className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
-              onHoverStart={() => setIsHoveredPro(true)}
-              onHoverEnd={() => setIsHoveredPro(false)}
-              animate={{
-                rotateZ: isHoveredPro ? 0 : 12,
-                rotateY: isHoveredPro ? 0 : -20,
-                rotateX: isHoveredPro ? 0 : 30,
-                scale: isHoveredPro ? 1 : 0.8, // Ajustar escala en función del hover
-                boxShadow: isHoveredPro && !isResponsive ? '0px 0px 200px rgba(255, 255, 255, 0.9)' : '2px 8px 10px rgba(255, 255, 255, 0.9)', // Cambiar sombra
-              }}
-            >
-              <div className="bg-black rounded-xl z-10">
-                <Image
-                  className="dark:invert w-[120px] h-[90px]"
-                  src="/home/proyect.svg"
-                  alt="my photo"
-                  width={120}
-                  height={100}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col gap-2 px-2">
-                <p className="text-md text-[#58e6d9]"><strong>Proyectos</strong></p>
-                <p>Todos los proyectos involucrados</p>
-              </div>
-            </motion.article>
 
+            {/* {PROYECTS} */}
+            <Link href="/pages/proyects" index className="w-full">
+              <motion.article
+                className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
+                onHoverStart={() => setIsHoveredPro(true)}
+                onHoverEnd={() => setIsHoveredPro(false)}
+                animate={{
+                  rotateZ: isHoveredPro ? 0 : 12,
+                  rotateY: isHoveredPro ? 0 : -20,
+                  rotateX: isHoveredPro ? 0 : 30,
+                  scale: isHoveredPro ? 1 : 0.8, // Ajustar escala en función del hover
+                  boxShadow: isHoveredPro && !isResponsive ? '0px 0px 200px rgba(255, 255, 255, 0.9)' : '2px 8px 10px rgba(255, 255, 255, 0.9)', // Cambiar sombra
+                }}
+              >
+                <div className="bg-black rounded-xl z-10">
+                  <Image
+                    className="dark:invert w-[120px] h-[90px]"
+                    src="/home/proyect.svg"
+                    alt="my photo"
+                    width={100}
+                    height={100}
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col gap-2 px-2">
+                  <p className="text-md text-[#58e6d9]"><strong>Proyectos</strong></p>
+                  <p>Todos los proyectos involucrados</p>
+                </div>
+              </motion.article>
+            </Link>
 
-            <motion.article
-              className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
-              onHoverStart={() => setIsHoveredAbo(true)}
-              onHoverEnd={() => setIsHoveredAbo(false)}
-              animate={{
-                rotateZ: isHoveredAbo ? 0 : 12,
-                rotateY: isHoveredAbo ? 0 : 20,
-                rotateX: isHoveredAbo ? 0 : -8,
-                scale: isHoveredAbo ? 1 : 0.8, // Ajustar escala en función del hover
-                boxShadow: isHoveredAbo && !isResponsive? '0px 0px 200px rgba(255, 255, 255, 0.9)' : '2px 8px 10px rgba(255, 255, 255, 0.9)', // Cambiar sombra
-              }}
-            >
-              <div className="bg-black rounded-xl z-10">
-                <Image
-                  className="dark:invert w-[120px] h-[90px]"
-                  src="/home/about.svg"
-                  alt="my photo"
-                  width={120}
-                  height={100}
-                  priority
-                />
-              </div>
-              <div className="flex flex-col gap-2 px-2">
-                <p className="text-md text-[#58e6d9]"><strong>Sobre mí</strong></p>
-                <p>Para conocerme un poco más</p>
-              </div>
-            </motion.article>
+            {/* {ABOUT} */}
+            <Link href="/pages/about" index className="w-full">
+              <motion.article
+                className="flex border border-white border-solid rounded-xl w-full p-2 z-10 bg-black transition-all duration-100 transform cursor-pointer"
+                onHoverStart={() => setIsHoveredAbo(true)}
+                onHoverEnd={() => setIsHoveredAbo(false)}
+                animate={{
+                  rotateZ: isHoveredAbo ? 0 : 12,
+                  rotateY: isHoveredAbo ? 0 : 20,
+                  rotateX: isHoveredAbo ? 0 : -8,
+                  scale: isHoveredAbo ? 1 : 0.8, // Ajustar escala en función del hover
+                  boxShadow: isHoveredAbo && !isResponsive ? '0px 0px 200px rgba(255, 255, 255, 0.9)' : '2px 8px 10px rgba(255, 255, 255, 0.9)', // Cambiar sombra
+                }}
+              >
+                <div className="bg-black rounded-xl z-10">
+                  <Image
+                    className="dark:invert w-[120px] h-[90px]"
+                    src="/home/about.svg"
+                    alt="my photo"
+                    width={100}
+                    height={100}
+                    priority
+                  />
+                </div>
+                <div className="flex flex-col gap-2 px-2">
+                  <p className="text-md text-[#58e6d9]"><strong>Sobre mí</strong></p>
+                  <p>Para conocerme un poco más</p>
+                </div>
+              </motion.article>
+            </Link>
+
           </div>
-
         </section>
 
         {/* Footer  */}
